60c0e6e04e8dfc9b8d094097e375cae0a563a6c4
Guilherme Leonardo Utzig <guilherme.utzig@maqina.com.br>
2018-03-01 14:07:39 -0300

    Ciclos de pagamento

---
+++ ciclos_de_pagamento/backend/src/api/billingCycle/billingCycleService.js
@@ -0,0 +1,44 @@
+const BillingCycle = require('./billingCycle')
+
+BillingCycle.methods(['get', 'post', 'put', 'delete'])
+BillingCycle.updateOptions({ new: true, runValidators: true })
+// New serve para trazer os novos dados em um update, o runValidators é para validar os itens do comando put
+
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if (error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json({value})
+        }
+    })
+})
+
+BillingCycle.route('summary', (req, res, next) => {
+    BillingCycle.aggregate({
+        $project: {
+            credit: {$sum: "$credits.value"},
+            debt: {$sum: "$debts.value"}
+        }
+    }, {
+        $group: {
+            _id: null,
+            credit: {$sum: "$credit"},
+            debt: {$sum: "$debt"}
+        }
+    }, {
+        $project: {
+            _id: 0,
+            credit: 1,
+            debt: 1
+        }
+    }, (error, result) => {
+        if (error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json(result[0] || {credit: 0, debt: 0})
+        }
+    })
+})
+
+module.exports = BillingCycle
