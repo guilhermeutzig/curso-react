f600fa77b872bd642dbc135094ae59ebbca1d7ea
Guilherme Leonardo Utzig <guilherme.utzig@maqina.com.br>
2018-03-13 13:13:17 -0300

    Progresso

--- ciclos_de_pagamento/frontend/src/index.jsx
+++ ciclos_de_pagamento/frontend/src/index.jsx
@@ -9,6 +9,7 @@ import App from './main/app'
 import CSS from './../application.styl'
 import reducers from './main/reducers'
 
+// Essa variável devTools serve para aparecer as opções do Redux no console do Chrome
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 const store = applyMiddleware(promise)(createStore)(reducers, devTools)
--- ciclos_de_pagamento/frontend/src/main/routes.jsx
+++ ciclos_de_pagamento/frontend/src/main/routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Router, Route, Redirect, hashHistory } from 'react-router'
 
+import DashboardNoRedux from '../components/dashboardNoRedux/dashboardNoRedux'
 import Dashboard from '../components/dashboard/dashboard'
 import BillingCycle from '../components/billingCycle/billingCycle'
 
@@ -8,6 +9,7 @@ export default props => (
 
     <Router history={hashHistory}>
         <Route path='/' component={Dashboard} />
+        <Route path='/noRedux' component={DashboardNoRedux} />
         <Route path='/billingCycles' component={BillingCycle} />
         <Route from='*' to='/' />
     </Router>
